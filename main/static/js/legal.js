f95fe63f75839a5ba2f72623ff42d9198684974f
Joseph Ferugon <joegferguson@gmail.com>
2021-05-23 17:07:15 -0500

    legal page 50% complete

--- main/static/js/legal.js
+++ main/static/js/legal.js
@@ -6,3 +6,14 @@ function info_consent(consentCheckBox) {
         document.getElementById("next_button").disabled = true;
     }
 }
+
+function serviceModalDisplay() {
+    document.getElementById("service-modal").style.display = "flex";
+    document.getElementById("service-modal").style.tabindex = "0";
+}
+
+function serviceModalClose() {
+    document.getElementById("service-modal").style.display = "none";
+    document.getElementById("service-modal").style.tabindex = "-1";
+
+}
